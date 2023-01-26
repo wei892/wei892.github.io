c48eb18e431972da114bbb7617c892b0fcb33eac
Wei <wlin.li2403@gmail.com>
2023-01-25 22:05:09 -0500

    edited homepage appearance

--- SMS_TOOL/homepage.js
+++ SMS_TOOL/homepage.js
@@ -1,48 +1,100 @@
-const APILINK = 'http://localhost:8000/api/v1/tool/';
+const APILINK = 'https://yearbackend.wli2424.repl.co/api/v1/year/';
 
-const list = document.getElementById("list");
-const addNew = document.getElementById("addNew");
+const main = document.getElementById("add");
+const title = document.getElementById("title");
+const list = document.getElementById("section");
 
-const div_new = document.createElement("div");
-
+const div_new = document.createElement('div');
 div_new.innerHTML = `
     <div class="column">
       <div class="card">
-        Add Year 
-        <input type="text" id="yearList" value=""> 
-        <a href="#" onclick="saveYear('new_year')" class="button"> Save </a>
+          Add Year 
+					<input type="text" id="new_year" value="">
+					<a href="#" onclick="saveYear('new_year')">Save </a>
       </div>
     </div>
-  </div>
 `
-addNew.appendChild(div_new)
+main.appendChild(div_new);
 
 
 returnYears(APILINK);
-function returnYear(url){
-    fetch(url).then(res => res.json())
-    .then (function(data){
-        console.log(data.results)
-        data.results.forEach(year => {
-            const yearNum = document.createElement('div');
-            yearNum.innerHTML =`
-            <div class="column">
-              <div class="card" id="${year._id}">
-              <p>${year.year} <a href="#"onclick="editReview('${year._id}','${year.year}'')">Edit</a> 
-              <a href="#" onclick="deleteReview('${year._id}')">Delete</a> 
-              </p>
-              </div>
-            </div>
-          </div>
-        `
-            list.appendChild("yearNum");
-        });
-    });
+
+function returnYears(url) {
+	fetch(url).then(res => res.json())
+		.then(function(data) {
+			console.log(data);
+			data.forEach(years => {
+				const div_card = document.createElement('div');
+				div_card.innerHTML = `
+				<div class="column" id="${years._id}">
+					<div class="card">
+		 			<b> ${years.year} </b> <br>
+					<div class="link">
+						<a href="region.html?year=${years.year}"> Link to Regions for ${years.year} </a> <br>
+	 				<div>
+		 			<div class = "buttons">
+						<a href = "#" onclick="editYear('${years._id}','${years.year}')" >Edit</a>
+						|
+		 				<a href = "#" onclick="deleteYear('${years._id}')" >Delete</a>
+	 				</div>
+					</div>
+				</div>`
+				list.appendChild(div_card);
+			});
+		});
 }
 
-// // form.addEventListener("submit", (e) => {
-// //     e.preventDefault();
+function editYear(id, year) {
+  const element = document.getElementById(id);
+  const yearInputId = "year" + id
 
-    
-// // })
+  element.innerHTML = `
+		<div class = "edit">
+			<p><strong>Year: </strong>
+				<input type="text" id="${yearInputId}" value="${year}"> 
+				<a href="#" onclick="saveYear('${yearInputId}', '${id}',)"> Save </a>
+		</div>
+		`
+}
 
+function saveYear(yearInputId, id = "") {
+	const year = document.getElementById(yearInputId).value;
+
+	if (id) {
+		fetch(APILINK + id, {
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json, text/plain, */*',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ "year": year })
+		}).then(res => res.json())
+			.then(res => {
+				console.log(res)
+				location.reload();
+			});
+	} else {
+		fetch(APILINK + "new", {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json, text/plain, */*',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ "year": year })
+		}).then(res => res.json())
+			.then(res => {
+				console.log(res)
+				location.reload();
+			});
+	}
+}
+
+function deleteYear(id) {
+  fetch(APILINK + id, {
+    method: 'DELETE'
+  }).then(res => res.json())
+    .then(res => {
+      console.log(res)
+      location.reload();
+    });    
+}
